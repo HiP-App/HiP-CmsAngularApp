8c131474fe719981d2bdf161a5f195de0dd76667
maltehol <maltehol@mail.upb.de>
2016-06-14 15:05:48 +0200

    HIPCMS-245 adding notification dropdown and language switcher

--- app/shared/sidenav/sidenav.component.ts
+++ app/shared/sidenav/sidenav.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
+import { ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 import { MdButton } from '@angular2-material/button';
 import { MdList, MD_LIST_DIRECTIVES } from '@angular2-material/list';
 import { MdSidenav, MdSidenavLayout } from '@angular2-material/sidenav';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
-import { MyTopicsComponent } from '../../topics/my-topics-list.component';
 import { ToolbarComponent } from './../toolbar/toolbar.component';
 import { FooterComponent } from './../footer/footer.component';
 
@@ -24,19 +23,6 @@ import { FooterComponent } from './../footer/footer.component';
     ],
     providers: [ROUTER_PROVIDERS]
 })
-@RouteConfig([
-    {
-        path: '/my-topics',
-        name: 'MyTopics',
-        component: MyTopicsComponent
-    },
-    {
-        path: '/dashboard',
-        name: 'Dashboard',
-        component: DashboardComponent,
-        useAsDefault: true
-    },
-])
 export class SidenavComponent {
     navigation = [
         {
--- app/shared/toolbar/toolbar.component.ts
+++ app/shared/toolbar/toolbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { HTTP_PROVIDERS } from '@angular/http';
+import { ROUTER_DIRECTIVES } from '@angular/router-deprecated';
 import { MdToolbar } from '@angular2-material/toolbar';
 import { MdButton } from '@angular2-material/button';
 import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
@@ -10,24 +11,48 @@ import { MdSidenav } from '@angular2-material/sidenav';
   viewProviders: [MdIconRegistry, HTTP_PROVIDERS],
   templateUrl: './app/shared/toolbar/toolbar.component.html',
   styleUrls: ['./app/shared/toolbar/toolbar.component.css'],
-  directives: [MdToolbar, MdButton, MdIcon]
+  directives: [MdToolbar, MdButton, MdIcon, ROUTER_DIRECTIVES]
 })
 export class ToolbarComponent {
-    @Input()
-    start = MdSidenav;
-    title = 'HiPCMS';
-    topnav = [
-        {
-            'href': '/contact/',
-            'name': 'contact'
-        },
-        {
-            'href': '/help/',
-            'name': 'help'
-        },
-        {
-            'href': '/about/',
-            'name': 'about'
-        }
-    ];
+  @Input()
+  start = MdSidenav;
+  title = 'HiPCMS';
+  notifications = [
+    {
+      'id': '123',
+      'link': 'MyTopics',
+      'title': 'Re: meeting',
+      'message': 'I will be there at 10. Would be nice to see you there.I bring coffee and cookies.'
+    },
+    {
+      'id': '122',
+      'link': 'MyTopics',
+      'title': 'you were mentioned',
+      'message': 'bjorn mentioned you in "Domplatz"'
+    },
+    {
+      'id': '111',
+      'link': 'MyTopics',
+      'title': 'you got a Grade',
+      'message': 'your text "Paderquellgebiet" was graded'
+    }
+  ];
+  languages = [
+    {
+      active: true,
+      short: 'EN',
+      name: 'english'
+    },
+    {
+      active: false,
+      short: 'DE',
+      name: 'deutsch'
+    }
+  ];
+
+  toggleLang() {
+    for (let lang of this.languages) {
+      lang.active = !lang.active;
+    }
+  };
 }
