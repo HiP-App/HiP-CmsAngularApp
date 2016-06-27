c7b74cc0bb0418fb9dcfca60c52d9ef7f2c7b1d9
Kai Malte Hollmann <maltehol@mail.uni-paderborn.de>
2016-06-27 10:14:03 +0200

    HIPCMS-108 adding content field and model binding

--- app/topics/shared/topic.model.ts
+++ app/topics/shared/topic.model.ts
@@ -1,10 +1,12 @@
 export class Topic {
-  title:string = '';
-  students:string = '';
-  reviewer:string = '';
-  requirements:string = '';
-  status:string = '';
-  supervisor:string = '';
-  content:string = '';
-  subTopics:Topic[] = [];
+  title: string = '';
+  students: string = '';
+  reviewer: string = '';
+  requirements: string = '';
+  status: string = '';
+  supervisor: string = '';
+  content: string = '';
+  dueDate: Date = new Date('today');
+  subTopics: Topic[] = [];
+  parentTopics: Topic[] = [];
 }
