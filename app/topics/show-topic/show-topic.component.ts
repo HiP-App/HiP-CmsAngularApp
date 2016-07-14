deccf88bb6a5b86f1219128f5efa8b2cf609b485
maltehol <maltehol@mail.upb.de>
2016-07-14 20:01:13 +0200

    HIPCMS-388 adding remove function and animation

--- app/topics/show-topic/show-topic.component.ts
+++ app/topics/show-topic/show-topic.component.ts
@@ -5,7 +5,12 @@ import { MdButton } from '@angular2-material/button';
 import { MdCard } from '@angular2-material/card';
 import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
 import { MdInput, MdHint } from '@angular2-material/input';
-import { MD_RADIO_DIRECTIVES, MdRadioGroup, MdRadioButton, MdUniqueSelectionDispatcher } from '@angular2-material/radio';
+import {
+  MD_RADIO_DIRECTIVES,
+  MdRadioGroup,
+  MdRadioButton,
+  MdUniqueSelectionDispatcher
+} from '@angular2-material/radio';
 
 import { TextareaComponent } from '../../shared/textarea/textarea.component';
 import { Topic } from '../index';
@@ -36,14 +41,17 @@ export class ShowTopicComponent implements OnInit {
   @Input() depth = 0;
   @Input() showContent = true;
   @Input() topic: Topic = Topic.emptyTopic();
+  @Input() currentParent: Topic = null;
   students = '';
   subTopics: Topic[] = this.topic.subTopics;
   dueDateString: string;
+  playAnimation = !this.showContent;
 
   ngOnInit() {
     if (typeof(this.topic.deadline) === Date.toString()) {
       this.dueDateString = this.topic.deadline.toISOString().slice(0, 10);
     }
+    this.playAnimation = !this.showContent;
   }
 
   toggleContent() {
@@ -51,11 +59,20 @@ export class ShowTopicComponent implements OnInit {
   };
 
   addSubTopic() {
-    this.subTopics.push(Topic.emptyTopic());
+    this.subTopics.push(Topic.emptyTopic([this.topic]));
   }
 
   addTopic() {
 
+  }
+
+  removeTopic() {
+    if (this.currentParent !== null) {
+      let index = this.currentParent.subTopics.indexOf(this.topic, 0);
+      if (index > -1) {
+        this.currentParent.subTopics.splice(index, 1);
+      }
+    }
   }
 
 }
