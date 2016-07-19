dff4e9b194ff5695f1780a93296bcf5539361144
kjosifov <kjosifov@mail.uni-paderborn.de>
2016-07-19 19:37:04 +0200

    Contact component

--- app/contact/contact.component.ts
+++ app/contact/contact.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { MdCard } from '@angular2-material/card';
+import {Component, ViewEncapsulation} from '@angular/core';
+import {HTTP_PROVIDERS} from '@angular/http';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MdCard} from '@angular2-material/card';
+import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
+import {MdList, MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import {MdSidenav, MdSidenavLayout} from '@angular2-material/sidenav';
+
+import {MD_INPUT_DIRECTIVES} from '@angular2-material/input';
+
+import {enableProdMode} from '@angular/core';
+enableProdMode();
+//import {MdCheckbox} from '@angular2-material/checkbox';
+
 
 @Component({
+   // moduleId: module.id,
     selector: 'hip-contact',
-    template: `
-        <md-card class="topics">
-        </md-card>
-        `,
-    directives: [MdCard]
+    templateUrl: './app/contact/contact.component.html',
+    
+
+    directives: [MdCard, MdList, MD_LIST_DIRECTIVES, MdButton, MD_INPUT_DIRECTIVES],
+    viewProviders: [MdIconRegistry, HTTP_PROVIDERS],
 })
 export class ContactComponent {
-}
+  
+  }
+
+
--- app/help/help.component.ts
+++ app/help/help.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { MdCard } from '@angular2-material/card';
-
+import {AutocompleteComponent} from '../shared/autocomplete/autocomplete.component';
+ 
 @Component({
     selector: 'hip-help',
     template: `
         <md-card class="topics">
+
+        <hip-autocomplete> </hip-autocomplete>
         </md-card>
         `,
-    directives: [MdCard]
+    directives: [MdCard, AutocompleteComponent]
 })
 export class HelpComponent {
 }
+
+
+
+
