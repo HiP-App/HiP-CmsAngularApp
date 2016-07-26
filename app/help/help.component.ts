0bbc5342ad800e39e03dc59a0c4888e86d4978ed
kjosifov <kjosifov@mail.uni-paderborn.de>
2016-07-26 23:54:10 +0200

    Update help.component.ts

--- app/help/help.component.ts
+++ app/help/help.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { MdCard } from '@angular2-material/card';
-import {AutocompleteComponent} from '../shared/autocomplete/autocomplete.component';
- 
 @Component({
     selector: 'hip-help',
     template: `
         <md-card class="topics">
-
-        <hip-autocomplete> </hip-autocomplete>
         </md-card>
         `,
-    directives: [MdCard, AutocompleteComponent]
+    directives: [MdCard]
 })
 export class HelpComponent {
 }
