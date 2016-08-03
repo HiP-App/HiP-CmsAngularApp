4ac092f6a0ae2147b57ffa1917944633baf375fa
kjosifov <kjosifov@mail.uni-paderborn.de>
2016-08-03 14:44:10 +0200

    HiPCMS-387- Incorporating the reviews.

--- app/shared/autocomplete/autocomplete.component.ts
+++ app/shared/autocomplete/autocomplete.component.ts
@@ -1,17 +1,9 @@
-  import { Component, Input, ViewEncapsulation, ElementRef } from '@angular/core';
+  import { Component, ElementRef } from '@angular/core';
   import { User } from '../../shared/user/user.model';
-  import { UserService }      from '../../shared/user/user.service';
-  import { MdButton } from '@angular2-material/button';
-  import { MdIcon, MdIconRegistry } from '@angular2-material/icon';
-  import { MdSidenav, MdSidenavLayout } from '@angular2-material/sidenav';
-  import { Router, ROUTER_DIRECTIVES }  from '@angular/router';
+  import { UserService } from '../../shared/user/user.service';
   import { MdCard } from '@angular2-material/card';
-  import { HTTP_PROVIDERS } from '@angular/http';
-  import { MdList, MD_LIST_DIRECTIVES } from '@angular2-material/list';
-  import { MD_INPUT_DIRECTIVES } from '@angular2-material/input';
   import { FORM_DIRECTIVES } from '@angular/common';
   import { CmsApiService } from '../../shared/api/cms-api.service';
-  import { MdToolbar } from '@angular2-material/toolbar';
 
   /**
    * Autocomplete component which fetches all users from the existing system 
@@ -26,18 +18,17 @@
       '(keydown)': 'handleKeyDown($event)',
       '(keyup)': 'handleKeyDown($event)'
     },
-    templateUrl:'./app/shared/autocomplete/autocomplete.component.html',
+    templateUrl: './app/shared/autocomplete/autocomplete.component.html',
     styleUrls: ['./app/shared/autocomplete/autocomplete.component.css'],
     viewProviders: [MdCard],
     providers: [UserService, CmsApiService],
-    directives: [FORM_DIRECTIVES], 
-
+    directives: [FORM_DIRECTIVES],
   })
 
    export class AutocompleteComponent {
      public query = '';
      public names: string[] = [];
-     public filteredList:string[]  = [];
+     public filteredList: string[]  = [];
      selectedIdx: number;
      username = '';
      public users: User[] = [];
@@ -47,7 +38,6 @@
        this.selectedIdx = -1;
        this.getEmail();
        this.filteredList = [];
-   
     }
 
    /**
@@ -68,7 +58,7 @@
      */
     getNames(users: User[]) {
       for (let user of users) {
-        this.names.push(user.email)
+        this.names.push(user.email);
       }
     }
 
@@ -77,28 +67,31 @@
      * 
      */
     filter(event: any) {
-       if (this.query !== "") {
-         this.filteredList = this.names.filter(function (el:string) {
-           return (el.toLowerCase().substr(0,this.query.length) === this.query.toLowerCase()) == true;
-         }.bind(this));
+       if (this.query !== '') {
+         this.filteredList = this.names.filter(function (el: string) {
+           return (el.toLowerCase().substr(0,this.query.length) === this.query.toLowerCase()) === true;
+         }.bind(this)); 
+       
 
-        //keydown
-        if (event.keyCode == 40) {
-           if (this.selectedIdx + 1 != this.filteredList.length)
-            this.selectedIdx++;
+        // keydown
+        if (event.keyCode === 40) {
+           if (this.selectedIdx + 1 !== this.filteredList.length) {
+              this.selectedIdx++;
+            }
          }
 
-        //keyup
-        if (event.keyCode == 38) {
-           if (this.selectedIdx > 0)
+        // keyup
+        if (event.keyCode === 38) {
+           if (this.selectedIdx > 0) {
              this.selectedIdx--;
+            }
          }
 
-        //enter
-        if (event.keyCode == 13) {
+        // enter
+        if (event.keyCode === 13) {
            if (this.filteredList[this.selectedIdx] !== undefined) {
               this.select(this.filteredList[this.selectedIdx]);
-          }
+        }
       }
       } else {
           this.filteredList = [];
@@ -113,15 +106,14 @@
     this.query = item;
     this.filteredList = [];
      }
-
-
+     
    /**
      * Function for handling event-click.
      * 
-     */ 
+     */
   handleClick(event: any) {
-    var clickedComponent = event.target;
-    var inside = false;
+    let clickedComponent = event.target;
+    let inside = false;
         do {
           if (clickedComponent === this.elementRef.nativeElement) {
             inside = true;
@@ -133,14 +125,13 @@
         }
         this.selectedIdx = -1;
   }
-    
+  
    /**
      * Function for handling event-arrow keys.
      * 
      */ 
-
   handleKeyDown(event: any) {
-   if (event.keyCode == 40 || event.keyCode == 38) {
+   if (event.keyCode === 40 || event.keyCode === 38) {
         event.preventDefault();
       }
     }
