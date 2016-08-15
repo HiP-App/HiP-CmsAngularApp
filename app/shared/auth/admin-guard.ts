3539736aa948fd8f01350ae8ac6cc8d208e1d447
Kai Malte Hollmann <maltehol@mail.uni-paderborn.de>
2016-08-15 10:16:20 +0200

    HIPCMS-405 fixing errors on build

--- app/shared/auth/admin-guard.ts
+++ app/shared/auth/admin-guard.ts
@@ -7,27 +7,51 @@ import {
 } from '@angular/router';
 
 import { UserService } from './../user/user.service';
+import { User } from '../user/user.model';
+import { CmsApiService } from '../api/cms-api.service';
+import { Observable } from 'rxjs/Rx';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-  constructor(private userService: UserService, private router: Router) { }
+  constructor(private cmsApiService: CmsApiService, private router: Router) {
+  }
 
   canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    this.userService.getCurrent().then(
-      user => {
-        if (user.role === 'Administrator') {
-          return true;
-        } else {
-          this.router.navigate(['/dashboard']);
-          return false;
+    return this.cmsApiService.getUrl('/api/Users/Current', {})
+      .map(
+        value => {
+          if ((<User> value.json()).role === 'Administrator') {
+            return true;
+          } else {
+            this.router.navigate(['/dashboard']);
+            return false;
+          }
         }
-      }
-    ).catch(
-      error => {
-        console.log(error);
-        this.router.navigate(['/dashboard']);
-        return false;
-      }
-    );
+      ).catch(this.handleError);
+    /*
+     this.userService.getCurrent().then(
+     user => {
+     if (user.role === 'Administrator') {
+     return true;
+     } else {
+     this.router.navigate(['/dashboard']);
+     return false;
+     }
+     }
+     ).catch(
+     error => {
+     console.log(error);
+     this.router.navigate(['/dashboard']);
+     return false;
+     }
+     ); */
+  }
+
+  handleError(error: any) {
+    console.log(error);
+    this.router.navigate(['/dashboard']);
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    return Observable.throw(errMsg);
   }
 }
