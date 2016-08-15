bc371a138855c6167946d88edbcf81c5579f755a
Lamija Halvadzija <lamija.halvadzija@gmail.com>
2016-08-15 14:27:12 +0200

    Email made as default search criterium and uncommented code in filter

--- app/admin/filter.pipe.ts
+++ app/admin/filter.pipe.ts
@@ -9,15 +9,14 @@ import { User } from '../shared/user/user.model';
 
 export class UsersFilter implements PipeTransform {
     transform(users: User[], query: string, criterium: string): User[] {
-        if (query !== undefined && users !== null) {
-            // if (criterium === 'First Name') {
-            //     return users.filter(user =>
-            //         (user.firstName.toLowerCase().indexOf(query.toLowerCase()) !== -1));
-            // } else if (criterium === 'Last Name' || criterium === undefined) {
-            //     return users.filter(user =>
-            //         (user.lastName.toLowerCase().indexOf(query.toLowerCase()) !== -1));
-            // } else
-            if (criterium === 'Email') {
+        if (query !== '' && query !== undefined && users !== null) {
+            if (criterium === 'First Name') {
+                return users.filter(user => 
+                    (user.firstName.toLowerCase().indexOf(query.toLowerCase()) !== -1));
+             } else if (criterium === 'Last Name') {
+                 return users.filter(user =>
+                     (user.lastName.toLowerCase().indexOf(query.toLowerCase()) !== -1));
+             } else if (criterium === 'Email' || criterium === undefined) {
                 return users.filter(user =>
                     (user.email.toLowerCase().indexOf(query.toLowerCase()) !== -1));
             } else if (criterium === 'Role') {
