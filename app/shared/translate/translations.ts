bba41dbd911a46b155a163d74f6acf1206a17564
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-08-22 13:36:14 +0200

    HIPCMS-413 Adding Translation Related Files

---
+++ app/shared/translate/translations.ts
@@ -0,0 +1,19 @@
+﻿import { OpaqueToken, provide } from '@angular/core';
+
+// import translations
+import { LANG_EN_NAME, LANG_EN_TRANS } from './lang-en';
+import { LANG_DE_NAME, LANG_DE_TRANS } from './lang-de';
+
+// translation token
+export const TRANSLATIONS = new OpaqueToken('translations');
+
+// all translations
+const dictionary = {
+    [LANG_EN_NAME]: LANG_EN_TRANS,
+    [LANG_DE_NAME]: LANG_DE_TRANS,
+};
+
+// providers
+export const TRANSLATION_PROVIDERS = [
+    provide(TRANSLATIONS, { useValue: dictionary }),
+];
