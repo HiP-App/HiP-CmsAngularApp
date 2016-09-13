3f87d9f3914c8fbff83bee85835c97a03142b709
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-09-13 22:19:16 +0200

    HIPCMS - 423 Adding Code comments for documentation
    Files which are missed in the previous commit

--- app/shared/taginput/taginput.component.ts
+++ app/shared/taginput/taginput.component.ts
@@ -13,10 +13,11 @@ import {
     templateUrl: './app/shared/taginput/taginput.component.html'
 })
 class TagInputComponent {
-    public students: number[] = [];
-    public users: User[] = [];
-    public errorMessage: any;
-    public names: string[] = [];
+
+    public students: number[] = []; // List of Students selected
+    public users: User[] = [];      // List for Holding Dynamic Users
+    public errorMessage: any;       // Handling error message
+    public names: string[] = [];    // AutoComplete List
 
     constructor(private userService: UserService) {
     }
@@ -25,6 +26,11 @@ class TagInputComponent {
         placeholder: '+ Tag'
     };
 
+
+    /**
+     * Callback for Adding Users from Auto complete List
+     * @param item represents the tag which is being added(by clicking enter or by mouse from dropdown)
+     */
     public onAdd(item: any) {
         this.userService.getUserId(item).then(
             data => this.setId(<User[]>data))
@@ -33,13 +39,18 @@ class TagInputComponent {
         );
     }
 
-    setId(users: User[]) {
+    public setId(users: User[]) {
         for (let user of users) {
             this.students.push(user.id);
         }
-        console.log(this.students);
+        //console.log(this.students);
     }
 
+
+    /**
+     * Callback for Removing Users from Tag input
+     * @param item represents the tag which is being removed
+     */
     public onRemove(item: any) {
         this.userService.getUserId(item).then(
             data => this.unsetId(<User[]>data))
@@ -48,84 +59,57 @@ class TagInputComponent {
          );
     }
 
-    unsetId(users: User[]) {
+    public unsetId(users: User[]) {
         for (let user of users) {
             this.students.splice(this.students.indexOf(user.id), 1);
         }
-        console.log(this.students);
+        //console.log(this.students);
     }
 
+
+    /**
+     * Callback when selecting a tag
+     * @param item represents the tag which is being currently selected
+     */
     public onSelect(item: any) {
         console.log(item + ' selected');
     }
 
-    public transform(item: string): string {
-        return `@${item}`;
-    }
 
+    /**
+     * Method which gets triggered on keyup i.e., when user tries to enter something on input
+     * @param event represents the keyup event
+     * Updates the list this.names which is fed to auto complete to have dynamic entries
+     */
     public updateStudentList(event: any) {
-        if (event.target.value.length <= 2 || event.keyCode === 40 || event.keyCode === 38) {
-            return;
-        }
-        this.userService.getUserNames(event.target.value, "Student").then(
-            data => this.getNames(<User[]>data))
-            .catch(
-            error => this.errorMessage = <any>error
-        );
+       if (event.target.value.length <= 2 || event.keyCode === 40 || event.keyCode === 38) {
+           return;
+       }
+       this.userService.getUserNames(event.target.value, "Student").then(
+           data => this.getNames(<User[]>data))
+           .catch(
+           error => this.errorMessage = <any>error
+       );
     }
 
-    getNames(users: User[]) {
+    public getNames(users: User[]) {
         this.names = [];
         for (let user of users) {
             this.names.push(user.email);
         }
-        console.log(this.names);
+        //console.log(this.names);
     }
-
-    private startsWithAt(control: FormControl) {
-        if (control.value.charAt(0) !== '@') {
-            return {
-                'startsWithAt@': true
-            };
-        }
-
-
-        return null;
-    }
-
-    private endsWith$(control: FormControl) {
-        if (control.value.charAt(control.value.length - 1) !== '$') {
-            return {
-                'endsWith$': true
-            };
-        }
-
-        return null;
-    }
-
-    public validators = [this.startsWithAt, this.endsWith$];
 
     public errorMessages = {
         'startsWithAt@': 'Your items need to start with "@"',
         'endsWith$': 'Your items need to end with "$"'
     };
 
-    public submitForm() {
-        //console.log(this.form);
-        console.log(this);
-        console.log('submitted')
-    }
 
-    private minimumLength(control: FormControl) {
-        if (!control.value.length) {
-            return {
-                'minimumLength': true
-            };
-        }
-
-        return undefined;
-    }
-
+    /**
+     * Callbacks for Focus and Blur
+     * @param $event
+     */
     public onBlur($event: any) {
         console.log("blur");
     }
