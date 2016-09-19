41d095549e6ce2894c1c2d9d8664fc011cd6ef5f
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-09-19 10:15:41 +0200

    HIPCMS-423 Update from 416 branch to avoid conflicts

--- app/topics/topics.module.ts
+++ app/topics/topics.module.ts
@@ -3,37 +3,44 @@ import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { MyTopicsComponent } from './my-topics-list/my-topics-list.component';
-import { NewTopicComponent } from './new-topic/new-topic.component';
-import { ShowTopicComponent } from './show-topic/show-topic.component';
+import { NewTopicComponent } from './topic-management/new-topic/new-topic.component';
 import { TopicTitleComponent } from './shared/topic-title.component';
 import { TopicService } from './shared/topic.service';
 import { topicRouting } from './topics.routing';
 import { MaterialModule } from '../material/material.module';
-import { SharedModule } from '../shared/shared.module';
 import { TextareaComponent } from '../shared/textarea/textarea.component';
+import { TopicInputComponent } from './topic-management/topic-input/topic-input.component';
+import { ShowTopicComponent } from './show-topic/show-topic.component';
+import { TranslateModule } from 'ng2-translate';
+import { TopicListComponent } from './shared/topic-list/topic-list.component';
+import { EditTopicComponent } from './topic-management/edit-topic/edit-topic.component';
 
 import TagInputComponent from '../shared/taginput/taginput.component';
 import { TagInputModule } from 'ng2-tag-input';
 
 @NgModule({
-  imports: [
-    BrowserModule,
-    topicRouting,
-    FormsModule,
-    ReactiveFormsModule,
-    MaterialModule,
-    TagInputModule
-  ],
-  declarations: [
-    MyTopicsComponent,
-    NewTopicComponent,
-    ShowTopicComponent,
-    TopicTitleComponent,
-    TextareaComponent,
-    TagInputComponent
-  ],
-  providers: [
-    TopicService
-  ]
+    imports: [
+        BrowserModule,
+        topicRouting,
+        FormsModule,
+        ReactiveFormsModule,
+        MaterialModule,
+        TranslateModule,
+        TagInputModule
+    ],
+    declarations: [
+        MyTopicsComponent,
+        NewTopicComponent,
+        TopicInputComponent,
+        TopicTitleComponent,
+        TextareaComponent,
+        ShowTopicComponent,
+        TopicListComponent,
+        EditTopicComponent,
+        TagInputComponent
+    ],
+    providers: [
+        TopicService,
+    ]
 })
-export class TopicModule {}
+export class TopicModule { }
