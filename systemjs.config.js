5d0e88673f88cfd19d57aa165ed9f62c49889477
venkatesh penchala <venkatesh.penchala@gmail.com>
2016-09-22 21:39:18 +0200

    HIPCMS - 423 Changing the Configuration for tagInput

--- systemjs.config.js
+++ systemjs.config.js
@@ -15,7 +15,7 @@
     'angular2-jwt': 'node_modules/angular2-jwt',
     'angular2-toaster': 'node_modules/angular2-toaster',
     'ng2-translate': 'node_modules/ng2-translate',
-    'ng2-tag-input': 'node_modules/ng2-tag-input'
+    'ng2-tag-input': 'node_modules/ng2-tag-input/dist/index.js'
   };
 
   // packages tells the System loader how to load when no filename and/or no extension
@@ -36,11 +36,6 @@
       format: 'cjs',
       defaultExtension: 'js',
       main: 'ng2-translate.js'
-    },
-    'ng2-tag-input': {
-        format: 'cjs',
-        defaultExtension: 'js',
-        main: 'dist/index.js'
     }
   };
 
@@ -89,8 +84,7 @@
 
   var packageNames = [
     'app/shared',
-    'app/shared/translate',
-    'app/shared/taginput'
+    'app/shared/translate'
   ];
 
   // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
