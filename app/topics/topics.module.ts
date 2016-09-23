efeb2d69004872f1214e13034dd6c86bab605c3c
Venkatesh Penchala <venkatesh.penchala@gmail.com>
2016-09-23 16:15:29 +0200

    HIPCMS - 423 Changing the order of imports to follow guidelines

--- app/topics/topics.module.ts
+++ app/topics/topics.module.ts
@@ -2,6 +2,9 @@ import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { TranslateModule } from 'ng2-translate';
+import { TagInputModule } from 'ng2-tag-input';
+
 import { MyTopicsComponent } from './my-topics-list/my-topics-list.component';
 import { NewTopicComponent } from './topic-management/new-topic/new-topic.component';
 import { TopicTitleComponent } from './shared/topic-title.component';
@@ -11,12 +14,10 @@ import { MaterialModule } from '../material/material.module';
 import { TextareaComponent } from '../shared/textarea/textarea.component';
 import { TopicInputComponent } from './topic-management/topic-input/topic-input.component';
 import { ShowTopicComponent } from './show-topic/show-topic.component';
-import { TranslateModule } from 'ng2-translate';
 import { TopicListComponent } from './shared/topic-list/topic-list.component';
 import { EditTopicComponent } from './topic-management/edit-topic/edit-topic.component';
 
 import { TagInputComponent } from '../shared/taginput/taginput.component';
-import { TagInputModule } from 'ng2-tag-input';
 import { AllTopicsComponent } from './all-topics/all-topics.component';
 import { TreeView } from './shared/treeview-node/treeview-node.component';
 
