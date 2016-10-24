688351d275e6e359b946131afc7374a17e46fda3
eschmidt1 <eschmidt1@users.noreply.github.com>
2016-10-24 17:41:39 +0200

    HIPCMS-441 bugfixes + cleanup
    bug 1: supervisors, reviewers and students randomly not loading when
    opening topic editor
    bug 2: 'tagPlaceholder' appears in input boxes instead of '+role'

--- app/topics/topic-management/edit-topic/edit-topic.component.ts
+++ app/topics/topic-management/edit-topic/edit-topic.component.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { Topic } from '../../shared/topic.model';
-import { TopicService } from '../../shared/topic.service';
+import { Component, Input, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { ToasterService } from 'angular2-toaster';
+
+import { Topic } from '../../shared/topic.model';
+import { TopicService } from '../../shared/topic.service';
 import { User } from '../../../core/user/user.model';
 
 @Component({
@@ -11,7 +12,7 @@ import { User } from '../../../core/user/user.model';
   styleUrls: ['./app/topics/topic-management/shared/save-topic-view.component.css']
 })
 export class EditTopicComponent implements OnInit {
-  topic: Topic = Topic.emptyTopic();
+  @Input() topic: Topic = Topic.emptyTopic();
 
   constructor(private topicService: TopicService,
               private router: Router,
@@ -20,62 +21,60 @@ export class EditTopicComponent implements OnInit {
   }
 
   ngOnInit() {
-    if (this.topic.deadline !== null) {
-      this.topic.deadline = this.topic.deadline.slice(0, 10);
-    }
     if (this.route.snapshot.url[0].path === 'topics' && this.route.snapshot.url[1].path === 'edit') {
       let id = +this.route.snapshot.params['id']; // (+) converts string 'id' to a number
+
       this.topicService.getTopic(id).then(
         (response: any) => {
           this.topic = <Topic> response;
           if (this.topic.deadline !== null) {
             this.topic.deadline = this.topic.deadline.slice(0, 10);
           }
+          this.fetchTopicDetails(id);
         }
       ).catch(
         (error: any) => this.toasterService.pop('error', 'Error fetching topic', error)
       );
-      this.topicService.getStudentsOfTopic(id).then(
-        (response: any) => {
-          console.log('fetched students');
-          this.topic.students = <User[]> response;
-          console.log(this.topic.students);
-          this.topic = this.topic;
-        }
-      ).catch(
-        (error: any) => this.toasterService.pop('error', 'Error fetching Students', error)
-      );
-      this.topicService.getReviewersOfTopic(id).then(
-        (response: any) => this.topic.reviewers = <User[]> response
-      ).catch(
-        (error: any) => this.toasterService.pop('error', 'Error fetching Reviewers', error)
-      );
-      this.topicService.getSupervisorsOfTopic(id).then(
-        (response: any) => this.topic.reviewers = <User[]> response
-      ).catch(
-        (error: any) => this.toasterService.pop('error', 'Error fetching Supervisors', error)
-      );
-      this.topicService.getSubTopics(id).then(
-        (response: any) => this.topic.subTopics = <Topic[]> response
-      ).catch(
-        (error: any) => this.toasterService.pop('error', 'Error fetching SubTopics', error)
-      );
     }
-    console.log(this.topic);
   }
 
-  public saveTopic() {
+  saveTopic() {
+    console.log('this will be saved: ');
     console.log(this.topic);
-    this.topicService.updateTopic(this.topic)
-      .then(
-        (response: any) => this.handleResponseUpdate()
-      )
-      .catch(
-        (error: any) => this.toasterService.pop('error', 'Error while saving', error)
-      );
+    this.topicService.updateTopic(this.topic).then(
+      (response: any) => this.handleResponseUpdate()
+    ).catch(
+      (error: any) => this.toasterService.pop('error', 'Error while saving', error)
+    );
+  }
+
+  private fetchTopicDetails(id: number) {
+    this.topicService.getStudentsOfTopic(id).then(
+      (response: any) => this.topic.students = <User[]> response
+    ).catch(
+      (error: any) => this.toasterService.pop('error', 'Error fetching Students', error)
+    );
+
+    this.topicService.getReviewersOfTopic(id).then(
+      (response: any) => this.topic.reviewers = <User[]> response
+    ).catch(
+      (error: any) => this.toasterService.pop('error', 'Error fetching Reviewers', error)
+    );
+
+    this.topicService.getSupervisorsOfTopic(id).then(
+      (response: any) => this.topic.supervisors = <User[]> response
+    ).catch(
+      (error: any) => this.toasterService.pop('error', 'Error fetching Supervisors', error)
+    );
+
+    this.topicService.getSubTopics(id).then(
+      (response: any) => this.topic.subTopics = <Topic[]> response
+    ).catch(
+      (error: any) => this.toasterService.pop('error', 'Error fetching SubTopics', error)
+    );
   }
 
   private handleResponseUpdate() {
-    this.toasterService.pop('success', 'Success', 'topic "' + this.topic.title + '" updated');
+    this.toasterService.pop('success', 'Success', 'Topic "' + this.topic.title + '" updated');
   }
 }
