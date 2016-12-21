dbadd47bb9a9b7109b609641eac5548c896c6860
eschmidt1 <eschmidt1@users.noreply.github.com>
2016-12-22 00:37:32 +0100

    First prototype of content annotation (#122)
    * HIPANGULAR-11 first working prototype
    
    Also includes a very basic implementation of the ability to delete
    annotations (HIPANGULAR-9). As of now annotations are deleted by simply
    clicking on them.
    
    * HIPANGULAR-11 make annotation topic-specific
    
    By request, the annotation component is now accessible from the 'Show
    Topic' component instead of main menu and now accepts topic ID as URL
    parameter.

--- app/tag-management/tag.module.ts
+++ app/tag-management/tag.module.ts
@@ -4,14 +4,16 @@ import { NgModule } from '@angular/core';
 import { TranslateModule } from 'ng2-translate';
 import { MaterialModule } from '@angular/material';
 
-import { TagRouting } from './tag.routing';
-import { AllTagsComponent } from './all-tags/all-tags.component';
-import { EditTagComponent } from './edit-tag/edit-tag.component';
-import { DeleteTagComponent } from './delete-tag/delete-tag.component';
-import { NewTagComponent } from './new-tag/new-tag.component';
-import { TagListComponent } from './all-tags/tag-list/tag-list.component';
-import { RemoveSubTagComponent } from './remove-sub-tags/remove-sub-tags.component';
-import { AddSubTagComponent } from './add-sub-tags/add-sub-tags.component';
+import { AddSubTagComponent } from  './add-sub-tags/add-sub-tags.component';
+import { AllTagsComponent } from  './all-tags/all-tags.component';
+import { AnnotationComponent } from  './annotation/annotation.component';
+import { EditTagComponent } from  './edit-tag/edit-tag.component';
+import { DeleteTagComponent } from  './delete-tag/delete-tag.component';
+import { NewTagComponent } from  './new-tag/new-tag.component';
+import { TagListComponent } from  './all-tags/tag-list/tag-list.component';
+import { RemoveSubTagComponent } from  './remove-sub-tags/remove-sub-tags.component';
+
+import { TagRouting } from  './tag.routing';
 import { TagService } from './tag.service';
 
 @NgModule({
@@ -25,6 +27,7 @@ import { TagService } from './tag.service';
   ],
   declarations: [
     AllTagsComponent,
+    AnnotationComponent,
     EditTagComponent,
     DeleteTagComponent,
     NewTagComponent,
