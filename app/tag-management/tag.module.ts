82a83c5edf9454143b13b8fb54a7306be4731fb5
maltehol <maltehol@mail.upb.de>
2016-12-21 23:06:24 +0100

    HIPANGULAR-35 replaces angular2-color-picker with an html5 one background: angular2-color-picker does not work with angular 2.3.1

--- app/tag-management/tag.module.ts
+++ app/tag-management/tag.module.ts
@@ -1,31 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MaterialModule } from '@angular/material';
 import { NgModule } from '@angular/core';
 import { TranslateModule } from 'ng2-translate';
+import { MaterialModule } from '@angular/material';
 
-import { AllTagsComponent } from  './all-tags/all-tags.component';
-import { EditTagComponent } from  './edit-tag/edit-tag.component';
-import { DeleteTagComponent } from  './delete-tag/delete-tag.component';
-import { NewTagComponent } from  './new-tag/new-tag.component';
-import { TagListComponent } from  './all-tags/tag-list/tag-list.component';
-import { RemoveSubTagComponent } from  './remove-sub-tags/remove-sub-tags.component';
-import { AddSubTagComponent } from  './add-sub-tags/add-sub-tags.component';
-import { TagRouting } from  './tag.routing';
+import { TagRouting } from './tag.routing';
+import { AllTagsComponent } from './all-tags/all-tags.component';
+import { EditTagComponent } from './edit-tag/edit-tag.component';
+import { DeleteTagComponent } from './delete-tag/delete-tag.component';
+import { NewTagComponent } from './new-tag/new-tag.component';
+import { TagListComponent } from './all-tags/tag-list/tag-list.component';
+import { RemoveSubTagComponent } from './remove-sub-tags/remove-sub-tags.component';
+import { AddSubTagComponent } from './add-sub-tags/add-sub-tags.component';
 import { TagService } from './tag.service';
-import { ColorPickerModule } from 'angular2-color-picker';
-
-
 
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
-    MaterialModule,
     ReactiveFormsModule,
     TagRouting,
     TranslateModule,
-    ColorPickerModule
+    MaterialModule,
   ],
   declarations: [
     AllTagsComponent,
@@ -34,7 +30,7 @@ import { ColorPickerModule } from 'angular2-color-picker';
     NewTagComponent,
     TagListComponent,
     RemoveSubTagComponent,
-    AddSubTagComponent 
+    AddSubTagComponent
   ],
   providers: [TagService]
 })
