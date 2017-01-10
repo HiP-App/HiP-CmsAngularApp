3c8ca87354a9409764ff9ef8c8f2feda8515e651
eschmidt1 <eschmidt1@users.noreply.github.com>
2017-01-10 12:39:09 +0100

    resolved merge conflicts

--- app/tag-management/tag.module.ts
+++ app/tag-management/tag.module.ts
@@ -4,16 +4,17 @@ import { MaterialModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { TranslateModule } from 'ng2-translate';
 
+import { tagRouting } from  './tag.routing';
 import { AddSubTagComponent } from  './add-sub-tags/add-sub-tags.component';
 import { AllTagsComponent } from  './all-tags/all-tags.component';
 import { AnnotationComponent } from  './annotation/annotation.component';
+import { CanvasComponent } from './annotation/canvas/canvas.component';
 import { DeleteTagComponent } from  './delete-tag/delete-tag.component';
 import { DeleteTagDialogComponent } from  './delete-tag-dialog/delete-tag-dialog.component';
 import { EditTagComponent } from  './edit-tag/edit-tag.component';
 import { NewTagComponent } from  './new-tag/new-tag.component';
 import { RemoveSubTagComponent } from  './remove-sub-tags/remove-sub-tags.component';
 import { TagListComponent } from  './all-tags/tag-list/tag-list.component';
-import { TagRouting } from  './tag.routing';
 import { TagService } from './tag.service';
 
 @NgModule({
@@ -21,16 +22,17 @@ import { TagService } from './tag.service';
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
-    TagRouting,
     TranslateModule,
     MaterialModule,
+    tagRouting
   ],
   declarations: [
     AllTagsComponent,
     AnnotationComponent,
-    EditTagComponent,
+    CanvasComponent,
     DeleteTagComponent,
     DeleteTagDialogComponent,
+    EditTagComponent,
     NewTagComponent,
     TagListComponent,
     RemoveSubTagComponent,
