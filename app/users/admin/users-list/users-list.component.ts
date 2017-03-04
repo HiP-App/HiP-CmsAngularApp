440bb44ae3f23cda326d1b6057910aad6e37a178
Nilakshi17 <nilakshi@mail.uni-paderborn.de>
2017-03-04 20:30:34 +0100

    Refactoring users-list component file

--- app/users/admin/users-list/users-list.component.ts
+++ app/users/admin/users-list/users-list.component.ts
@@ -2,6 +2,7 @@
 import { Observable } from 'rxjs';
 
 import { CmsApiService } from '../../../core/api/cms-api.service';
+import { UserService } from '../../../core/user/user.service';
 import { User } from '../../../core/user/user.model';
 import { Roles } from '../roles.model';
 
@@ -24,22 +25,37 @@ export class UsersListComponent implements OnInit {
   _page = 1;
   _total: number;
 
-  constructor(private cmsApiService: CmsApiService) {}
+  constructor(private cmsApiService: CmsApiService,
+              private userService: UserService) {}
 
   ngOnInit(): any {
     this.roles.push('all roles');
     this.getPage(1);
   }
 
+  // getPage(page: number) {
+  //   this._items = this.userService.getAll('/api/Users', {})
+  //     .do(
+  //       (res: any) => {
+  //         this._total = res.json().total;
+  //         this._page = page;
+  //       }
+  //     ).map(
+  //       (res: any) => res.json().items
+  //     );
+  // }
+
   getPage(page: number) {
-    this._items = this.cmsApiService.getUrl('/api/Users', {})
-      .do(
-        (res: any) => {
-          this._total = res.json().total;
+    this.userService.getAll()
+      .then(
+        (response: any) => {
+          console.log(response)
+          this._items = response;
+          this._total = response.total;
           this._page = page;
         }
-      ).map(
-        (res: any) => res.json().items
+      ).catch(
+        (error: any) => console.error(error)
       );
   }
 
