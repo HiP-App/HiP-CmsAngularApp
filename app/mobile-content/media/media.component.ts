5b2bab96ec83b07481e915428acc1448e62c0adf
eschmidt1 <eschmidt1@users.noreply.github.com>
2017-05-30 11:33:13 +0200

    HIPCMS-645: Media selection dialog (#502)
    * initial implementation
    
    * move medium model to shared folder
    
    * filter dialog by type + add it to routes component
    
    * Shorten imports

--- app/mobile-content/media/media.component.ts
+++ app/mobile-content/media/media.component.ts
@@ -1,91 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-import { MdDialog, MdDialogRef } from '@angular/material';
-
-import { DeleteMediumDialogComponent } from './delete-medium-dialog/delete-medium-dialog.component';
-import { EditMediumDialogComponent } from './edit-medium-dialog/edit-medium-dialog.component';
-import { Medium } from './medium.model';
-import { Status } from '../shared/status.model';
-import { UploadMediumDialogComponent } from './upload-medium-dialog/upload-medium-dialog.component';
+import { Component } from '@angular/core';
 
 @Component({
   moduleId: module.id,
   selector: 'hip-media',
-  styleUrls: ['media.component.css'],
   templateUrl: 'media.component.html'
 })
-export class MediaComponent implements OnInit {
-  media: Medium[];
-  statuses = Status.getValuesForSearch();
-  types = ['ALL'].concat(Medium.types);
-
-  // search parameters
-  searchQuery = '';
-  selectedStatus = 'ALL';
-  selectedType = 'ALL';
-  showingSearchResults = false;
-
-  // pagination parameters
-  currentPage = 1;
-  pageSize = 10;
-  totalItems: number;   // must be fetched from server
-
-  // dialogs
-  private deleteDialogRef: MdDialogRef<DeleteMediumDialogComponent>;
-  private editDialogRef: MdDialogRef<EditMediumDialogComponent>;
-  private uploadDialogRef: MdDialogRef<UploadMediumDialogComponent>;
-
-  constructor(private dialog: MdDialog) {}
-
-  ngOnInit() {
-    this.media = new Array(30);
-    this.totalItems = this.media.length;
-    for (let i = 0; i < this.media.length; i++) {
-      this.media[i] = Medium.getRandom();
-    }
-  }
-
-  addMedium() {
-    this.uploadDialogRef = this.dialog.open(UploadMediumDialogComponent, { width: '35em' });
-  }
-
-  deleteMedium(medium: Medium) {
-    this.deleteDialogRef = this.dialog.open(DeleteMediumDialogComponent);
-    this.deleteDialogRef.componentInstance.mediumTitle = medium.title;
-    this.deleteDialogRef.afterClosed().subscribe(
-      (confirmed: boolean) => {
-        if (confirmed) {
-          // TODO: implement medium deletion
-        }
-      }
-    );
-  }
-
-  editMedium(medium: Medium) {
-    this.editDialogRef = this.dialog.open(EditMediumDialogComponent, { width: '30em', data: { medium: medium } });
-    this.editDialogRef.afterClosed().subscribe(
-      (newMedium: Medium) => {
-        if (newMedium) {
-          // TODO: save edited medium
-        }
-      }
-    );
-  }
-
-  findMedia() {
-    // TODO: implement media search
-    this.showingSearchResults = true;
-  }
-
-  getPage(page: number) {
-    this.currentPage = page;
-  }
-
-  reloadList() {
-    // TODO: implement list reload
-  }
-
-  resetSearch() {
-    this.showingSearchResults = false;
-    this.searchQuery = '';
-  }
+export class MediaComponent {
 }
