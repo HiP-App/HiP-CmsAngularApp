e65a3648626fee5c9adc04b9000da345fdee778d
Patrick Robrecht <mail@patrick-robrecht.de>
2017-08-07 14:45:51 +0200

    Bugfix for login tests

--- app/authentication/login/login.component.e2e-spec.ts
+++ app/authentication/login/login.component.e2e-spec.ts
@@ -22,35 +22,37 @@ describe('Login', () => {
     }, 10000);
   });
 
-  it('should have input fields and a submit button', () => {
-    browser.wait(() => {
-      submitButton = element(by.id('login-button'));
-      expect(submitButton.isDisplayed()).toEqual(true);
-    }, 10000);
+  it('should have a submit button', () => {
+    submitButton = element(by.id('login-button'));
+    expect(submitButton.isDisplayed()).toEqual(true);
   });
 
   it('login test user', () => {
-    submitButton.click().then(() => {
-      browser.wait(() => {
-        let emailInput = element(by.css('input[type="email"]'));
-        let passwordInput = element(by.css('input[type="password"]'));
+    submitButton.click()
+      .then(
+        () => browser.sleep(10000)
+      ).then(
+        () => {
+          let emailInput = element(by.css('input[type="email"]'));
+          let passwordInput = element(by.css('input[type="password"]'));
 
-        expect(emailInput.isDisplayed()).toEqual(true);
-        expect(passwordInput.isDisplayed()).toEqual(true);
+          expect(emailInput.isDisplayed()).toEqual(true);
+          expect(passwordInput.isDisplayed()).toEqual(true);
 
-        emailInput.sendKeys(testDataJson.username);
-        passwordInput.sendKeys(testDataJson.password);
+          emailInput.sendKeys(testDataJson.username);
+          passwordInput.sendKeys(testDataJson.password);
 
-        let loginButton = element(by.css('.auth0-label-submit'));
-
-        return loginButton.click();
-      }, 20000);
-    }).then(() => {
-      browser.wait(() => {
-        return browser.getCurrentUrl().then(function (url) {
-          return (/dashboard/).test(url);
-        });
-      }, 20000);
-    });
+          let loginButton = element(by.css('.auth0-label-submit'));
+          loginButton.click();
+        }
+      ).then(
+        () => {
+          browser.wait(() => {
+            return browser.getCurrentUrl().then(function (url) {
+              return (/dashboard/).test(url);
+            });
+          }, 20000);
+        }
+      );
   });
 });
