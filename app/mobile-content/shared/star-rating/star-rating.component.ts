834d716af03043c37106729d0f94666b557d1d22
Rajat Khanna <rajat@mail.uni-paderborn.de>
2017-10-23 04:11:04 +0200

    Added comments and made minor changes to the exhibits star rating code

--- app/mobile-content/shared/star-rating/star-rating.component.ts
+++ app/mobile-content/shared/star-rating/star-rating.component.ts
@@ -19,26 +19,31 @@ export class RatingComponent implements OnChanges {
     if (this.rating.toString().split('.')[0] !== '0') {
       for (let y = 1; y <= Number(this.rating.toString().split('.')[0]); y++) {
         this.range.push({rating: y, iconValue: 'star'});
+          // Coverts a floating point value into integer
         this._value = Math.floor(Number(this.rating));
       }
     }
+      // If we have no ratings for an exhibit, show five blank stars
     if (this.rating.toString().split('.')[0] === '0') {
       for (let y = 1; y <= 5; y++) {
         this.range.push({rating: y, iconValue: 'star_border'});
       }
     }
     if (this.rating.toString().split('.')[1]) {
+        // Shows half star ratings for each exhibit
       for (let y = 1; y <= 1; y++) {
         this.range.push({rating: this._value + y, iconValue: 'star_half'});
       }
     }
+      // Complete the remaining(blank) stars in case when we have a half star rating(floating point rating)
     if (this.rating.toString().split('.')[0] !== '0' && this.rating.toString().split('.')[1]) {
-      for (let y = 2; y <= 5 - Number(this.rating.toString().split('.')[0]); y++) {
+      for (let y =  Number(this.rating.toString().split('.')[0]); y < 4; y++) {
         this.range.push({rating: this._value + y, iconValue: 'star_border'});
       }
     }
+      // Complete the remaining(blank) stars in case when we have no half star rating
     if (this.rating.toString().split('.')[0] !== '0' && this.rating.toString().split('.')[1] === undefined) {
-      for (let y = 1; y <= 5 - Number(this.rating.toString().split('.')[0]); y++) {
+      for (let y = Number(this.rating.toString().split('.')[0]); y < 5; y++) {
         this.range.push({rating: this._value + y, iconValue: 'star_border'});
       }
     }
