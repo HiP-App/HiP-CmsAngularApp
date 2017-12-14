f06303a54967ae56902787ef1010e388a72dcf0e
Peter Schick <pschick@mail.uni-paderborn.de>
2017-12-14 19:58:30 +0100

    Add new component "star-rating-table" to exhibit view and route view

---
+++ app/mobile-content/shared/star-rating-table/star-rating-table.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'hip-star-rating-table',
+  templateUrl: 'star-rating-table.component.html',
+  styleUrls: ['star-rating-table.component.css']
+})
+
+export class RatingTableComponent implements OnInit {
+
+  @Input() rating: any;
+  ratingTable: any;
+  average: number;
+
+  ngOnInit() {
+    this.ratingTable = this.rating.ratingTable;
+    this.average = this.rating.average;
+  }
+}
