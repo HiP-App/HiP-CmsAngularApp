353bd1242e05b2c3c3dd1d0d25ffb6472c40f120
Ruchir <31822561+ruchirnaphade@users.noreply.github.com>
2018-03-12 14:03:21 +0100

    changed token (#772)

--- app/mobile-content/shared/mobile-content-api.service.ts
+++ app/mobile-content/shared/mobile-content-api.service.ts
@@ -63,7 +63,7 @@ export class MobileContentApiService {
   public putUrlWithFormData(apiUrl: string, data: any) {
     this.setUrl();
     let headers = new Headers();
-    headers.append('authorization', 'Bearer ' + localStorage.getItem('id_token'));
+    headers.append('authorization', 'Bearer ' + localStorage.getItem('access_token'));
     headers.append('Access-Control-Allow-Origin', '*');
     headers.append('Accept', 'application/json');
     return this.http.put(this.mobileContentApiUrl + apiUrl, data, {headers});
