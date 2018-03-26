25c4d602502038894b6558de32e480249b0c1852
Rajat Khanna <rajat@mail.uni-paderborn.de>
2018-03-26 06:25:16 +0200

    Signup page added

---
+++ app/authentication/signup/signup.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'signup',
+    templateUrl: 'signup.component.html',
+    styleUrls: ['signup.component.css']
+})
+export class SignupComponent {
+
+}
