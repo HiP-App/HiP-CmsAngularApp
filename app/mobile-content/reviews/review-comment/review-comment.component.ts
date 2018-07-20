f82c7181b6854fb7adb9d9f53d6c861ea4ebc6d6
Rajat Khanna <rajatkhanna311@gmail.com>
2018-07-20 03:50:22 +0200

    Added review-comment component and necassary icons for commenting and approving a review.

---
+++ app/mobile-content/reviews/review-comment/review-comment.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'review-comment',
+    templateUrl: 'review-comment.component.html',
+    styleUrls: ['review-comment.component.css']
+})
+export class ReviewCommentComponent {
+
+}
---
+++ app/mobile-content/reviews/review-comment/review-comment.module.ts
@@ -0,0 +1,20 @@
+// Angular Imports
+import { NgModule } from '@angular/core';
+
+// This Module's Components
+import { ReviewCommentComponent } from './review-comment.component';
+
+@NgModule({
+    imports: [
+
+    ],
+    declarations: [
+        ReviewCommentComponent,
+    ],
+    exports: [
+        ReviewCommentComponent,
+    ]
+})
+export class ReviewCommentModule {
+
+}
